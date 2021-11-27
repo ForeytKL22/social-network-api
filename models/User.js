0d0c1be861190f2c3cf11284ed128de195f5c30a
ForeytKL22 <krforeyt@gmail.com>
2021-11-26 18:09:21 -0700

    added user and thought models, and reaction subdocument

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = requir('mongoose');
+
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true,
+        required: 'This is not a valid username',
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: 'This is not a valid email address',
+        match: [/.+\@.+\..+/]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+
+// not sure about this?
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.reduce((total, friend) => total + friend.length + 1, 0);
+});
+
+
+const User = model('User', UserSchema);
+
+module.exports = User;
